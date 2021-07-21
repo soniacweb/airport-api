039f509b61271382e3647295e995700f290c0a49
Sonia Choudhury <sonia.choudhury@hotmail.co.uk>
2021-07-21 10:42:03 +0100

    update put test bug

--- app.js
+++ app.js
@@ -1,51 +1,44 @@
 const express = require("express");
 const airports = require("./airports");
-
 const app = express();
-
 app.use(express.json());
-
 // get all the airports
 app.get("/airports", (req, res) => {
     res.json(airports); // can also use res.send works the same, however json is more explicit
 });
-
 // create new airport
 app.post("/airports", (req, res) => {
     airports.push(req.body);
     console.log(airports);
     res.sendStatus(201);
 });
-
 // return the airport with the id specified in the URL
 app.get("/airports/:id", (req, res) => {
-    const singleAirportId = req.params.id 
-    const singleAirport = airports.find(airport => airport.icao == singleAirportId)
-    res.json(singleAirport)
+    const singleAirportId = req.params.id;
+    const singleAirport = airports.find(
+        (airport) => airport.icao === singleAirportId
+    );
+    res.json(singleAirport);
     res.sendStatus(201);
 });
-
 // update an airport
 app.put("/airports/:id", (req, res) => {
-    const singleAirportId = req.params.id // identifying specific id
-    const body = req.body // what you're passing to the endpoint
-    let airport = airports.find(airport => airport.icao == singleAirportId) // first id that matches with the searched id
-    const index = airports.indexOf(airport) //finds the array index of the argument that you pass in- where airport sitss in airports
-    airport = body // updating your changes into airport
-    airports[index] = airport // reassigning the updated airport
-    res.json(airport)
-    res.sendStatus(201);
+    const singleAirportId = req.params.id; // identifying specific id
+    const newAirportData = req.body; // what you're passing to the endpoint
+    let indexToReplace = airports.findIndex(
+        (airport) => airport.icao === singleAirportId
+    ); // first id that matches with the searched id
+    airports[indexToReplace] = newAirportData; // reassigning the updated airport
+    console.log(airports[indexToReplace]);
+    res.sendStatus(200);
+    // res.sendStatus(201);
 });
-
 // delete an airport
 app.delete("/airports/:id", (req, res) => {
-    const singleAirportId = req.params.id 
-    let airport = airports.find(airport => airport.icao == singleAirportId) // first id that matches with the searched id
-    const index = airports.indexOf(airport) //finds the array index of the argument that you pass in- where airport sitss in airports
-    airports.splice(airports[index], 1) // reassigning the updated airport
-
-    res.json(airports)
-    res.sendStatus(201);
+    const singleAirportId = req.params.id;
+    let airport = airports.find((airport) => airport.icao == singleAirportId); // first id that matches with the searched id
+    const index = airports.indexOf(airport); //finds the array index of the argument that you pass in- where airport sitss in airports
+    airports.splice(airports[index], 1); // reassigning the updated airport
+    res.json(airports);
 });
-
 module.exports = app;
--- app.test.js
+++ app.test.js
@@ -3,8 +3,6 @@ const airports = require("./airports");
 const app = require("./app");
 const request = require("supertest");
 
-// console.log('length', originalAirports.length)
-
 describe("get all airport tests", () => {
     test("GET /airports should return all airports", (done) => {
         request(app)
@@ -18,11 +16,10 @@ describe("get all airport tests", () => {
     });
     test("POST /airports should create the new airport", (done) => {
         const newAirport = {
-            "icao": "1234",
-            "name": "Sonias Airport",
-            "city": "LA",
-            "state": "Cal",
-
+            icao: "1234",
+            name: "Sonias Airport",
+            city: "LA",
+            state: "Cal",
         };
         const expectedAirportList = [...originalAirports];
         expectedAirportList.push(newAirport);
@@ -35,19 +32,18 @@ describe("get all airport tests", () => {
                 return done();
             });
     });
-
     test("GET /airports/:id should return specific airport", (done) => {
         const newAirport = {
-            "icao": "02WI",
-            "iata": "",
-            "name": "Beer Airport",
-            "city": "Hudson",
-            "state": "Wisconsin",
-            "country": "US",
-            "elevation": 920,
-            "lat": 45.0318984985,
-            "lon": -92.6557998657,
-            "tz": "America/Chicago"
+            icao: "02WI",
+            iata: "",
+            name: "Beer Airport",
+            city: "Hudson",
+            state: "Wisconsin",
+            country: "US",
+            elevation: 920,
+            lat: 45.0318984985,
+            lon: -92.6557998657,
+            tz: "America/Chicago",
         };
         request(app)
             .get("/airports/02WI")
@@ -58,74 +54,41 @@ describe("get all airport tests", () => {
                 done();
             });
     });
-
     test("PUT /airports/:id should update specific airport with my changes", (done) => {
-        let existingAirport = {
-            "icao": "02WI",
-            "iata": "",
-            "name": "Beer Airport",
-            "city": "Hudson",
-            "state": "Wisconsin",
-            "country": "US",
-            "elevation": 920,
-            "lat": 45.0318984985,
-            "lon": -92.6557998657,
-            "tz": "America/Chicago"
-        };
+       
         let updatedAirport = {
-            "icao": "02WI",
-            "iata": "",
-            "name": "Sonias Airport",
-            "city": "Slough",
-            "state": "Wisconsin",
-            "country": "US",
-            "elevation": 920,
-            "lat": 45.0318984985,
-            "lon": -92.6557998657,
-            "tz": "America/Chicago"
+            icao: "02WI",
+            iata: "",
+            name: "Sonias Airport",
+            city: "Slough",
+            state: "Wisconsin",
+            country: "US",
+            elevation: 920,
+            lat: 45.0318984985,
+            lon: -92.6557998657,
+            tz: "America/Chicago",
         };
         request(app)
             .put("/airports/02WI")
-            .expect("Content-Type", /json/)
-            .set(existingAirport = updatedAirport) // ask charlie if this is being used correctly
-            .send(existingAirport)
+            // .expect("Content-Type", /json/)
+            .set(updatedAirport) // ask charlie if this is being used correctly
+            // .send()
             .expect(200)
             .then((response) => {
                 // console.log(existingAirport)
-                expect(response.body).toEqual(existingAirport);
+                expect(updatedAirport).toEqual(airports);
                 done();
             });
     });
-
-    test("Delete /airports/:id should delete specific airport", (done) => {
-
-        let existingAirport = {
-            "icao": "02WI",
-            "iata": "",
-            "name": "Beer Airport",
-            "city": "Hudson",
-            "state": "Wisconsin",
-            "country": "US",
-            "elevation": 920,
-            "lat": 45.0318984985,
-            "lon": -92.6557998657,
-            "tz": "America/Chicago"
-        };
-
-        const expectedAirportList = [...originalAirports]; //create array from json
-        const find = expectedAirportList.find(airport => airport.icao == existingAirport.icao) // compare ids from array and chosen airport
-        const index = expectedAirportList.indexOf(find) // get the indexof from the matched id
+    test("Delete /airports/:id should delete specific airport", async () => {
+        const expectedAirportList = [...airports]; // duplicate database
+        const icaoToDelete = "00FL";
+        const index = expectedAirportList.findIndex(
+            (airport) => airport.icao === icaoToDelete
+        ); // compare ids from array and chosen airport
         expectedAirportList.splice(expectedAirportList[index], 1);
-
-        request(app)
-            .delete("/airports/02WI")
-            .expect("Content-Type", /json/)
-            .expect(200)
-            .then(() => {
-                console.log('hello', expectedAirportList[1].icao)
-                expect(expectedAirportList.length).toEqual(true);
-                done();
-            });
+        const response = await request(app).delete("/airports/" + icaoToDelete);
+        expect(response.status).toEqual(200);
+        expect(expectedAirportList).toEqual(airports);
     });
-
 });
